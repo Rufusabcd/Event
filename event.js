c61dcf8ad0bc7af636c7720862ce2579e1719f6a
Marcin Grzeniewski <MarcinGrzeniewski@MacBook-Air-Marcin.local>
2017-09-30 08:56:11 +0200

    eventchanges

--- event.js
+++ event.js
@@ -1,9 +1,10 @@
-var list = document.getElementById('list')
+var list = document.getElementById("list");
 
-var add = document.getElementsByTagName('button')
+var add = document.getElementById("addElem");
 
 add.addEventListener('click', function() {
-    var element = document.createElement('li');
-    element.innerHTML = 'item';
+    var element = document.createElement("li");
+    var number = document.getElementsByTagName("li").length
+    element.innerHTML = 'item' + ' ' + number;
     list.appendChild(element);
 });
